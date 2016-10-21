16b85c1ca7ae724913038b7560378cc42a264b16
Luca Stornaiuolo <storna93@live.it>
2016-10-22 00:34:26 +0200

    Splash Screen

---
+++ app/src/main/java/com/moscowmuleaddicted/neighborhoodsecurity/activity/HomePage.java
@@ -0,0 +1,14 @@
+package com.moscowmuleaddicted.neighborhoodsecurity.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.moscowmuleaddicted.neighborhoodsecurity.R;
+
+public class HomePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_page);
+    }
+}
--- app/src/main/java/com/moscowmuleaddicted/neighborhoodsecurity/activity/SplashActivity.java
+++ app/src/main/java/com/moscowmuleaddicted/neighborhoodsecurity/activity/SplashActivity.java
@@ -1,15 +1,30 @@
 package com.moscowmuleaddicted.neighborhoodsecurity.activity;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
-
-import com.moscowmuleaddicted.neighborhoodsecurity.R;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
 
 public class SplashActivity extends AppCompatActivity {
+
+    // Splash screen timer
+    private static int SPLASH_TIME_OUT = 2000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
+
+        // Start HomePage after SPLASH_TIME_OUT
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // Start SplashActivity once the timer is over
+                Intent intent = new Intent(SplashActivity.this, HomePage.class);
+                startActivity(intent);
+
+                // Close this Activity
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
     }
 }
