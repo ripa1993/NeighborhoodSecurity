77b1ae099e94642119c643aadb74852340598a48
ripa1993 <simone.ripamonti.1993@gmail.com>
2017-05-10 16:22:15 +0200

    Removing garbage

--- app/src/main/java/com/moscowmuleaddicted/neighborhoodsecurity/activity/EventListActivity.java
+++ app/src/main/java/com/moscowmuleaddicted/neighborhoodsecurity/activity/EventListActivity.java
@@ -40,7 +40,6 @@ public class EventListActivity extends AppCompatActivity implements EventListFra
         setContentView(R.layout.activity_event_list);
         Bundle extras = getIntent().getExtras();
 
-        boolean sample = true;
         ArrayList<Event> events = new ArrayList<>();
         if(extras != null) {
             if(extras.containsKey("event-list")){
@@ -104,39 +103,10 @@ public class EventListActivity extends AppCompatActivity implements EventListFra
 
     @Override
     public void onListFragmentInteraction(Event event) {
-//        Toast.makeText(getApplicationContext(), "clicked event "+event.getId(), Toast.LENGTH_SHORT).show();
         Intent intent = new Intent(this, EventDetailActivity.class);
         intent.putExtra("event", event);
         startActivity(intent);
 
-    }
-
-
-    private void generateSampleList(){
-        NSService.getInstance(getApplicationContext()).getEventsByArea(44d, 46d, 8d, 10d, new NSService.MyCallback<List<Event>>() {
-            @Override
-            public void onSuccess(List<Event> receivedEvents) {
-                ArrayList<Event> events = new ArrayList<>(receivedEvents);
-                EventListFragment mFragment = EventListFragment.newInstance(1, events);
-
-                // initialize the fragment
-                FragmentManager fragmentManager = getSupportFragmentManager();
-                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-                fragmentTransaction.add(R.id.event_list_fragment, mFragment);
-                fragmentTransaction.commit();
-
-            }
-
-            @Override
-            public void onFailure() {
-                Toast.makeText(getApplicationContext(), "fail", Toast.LENGTH_SHORT).show();
-            }
-
-            @Override
-            public void onMessageLoad(MyMessage message, int status) {
-                Toast.makeText(getApplicationContext(), "fail", Toast.LENGTH_SHORT).show();
-            }
-        });
     }
 
     @Override
