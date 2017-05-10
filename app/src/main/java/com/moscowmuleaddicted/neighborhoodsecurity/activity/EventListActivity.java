626315ec5fbaec28fa7068215fa52d6cb03f1bef
ripa1993 <simone.ripamonti.1993@gmail.com>
2017-05-10 15:26:59 +0200

    EventListActivity support for passed UID extra

--- app/src/main/java/com/moscowmuleaddicted/neighborhoodsecurity/activity/EventListActivity.java
+++ app/src/main/java/com/moscowmuleaddicted/neighborhoodsecurity/activity/EventListActivity.java
@@ -1,16 +1,20 @@
 package com.moscowmuleaddicted.neighborhoodsecurity.activity;
 
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
 import com.moscowmuleaddicted.neighborhoodsecurity.R;
+import com.moscowmuleaddicted.neighborhoodsecurity.adapter.MyEventRecyclerViewAdapter;
 import com.moscowmuleaddicted.neighborhoodsecurity.fragment.EventCreateFragment;
 import com.moscowmuleaddicted.neighborhoodsecurity.fragment.EventListFragment;
 import com.moscowmuleaddicted.neighborhoodsecurity.fragment.SubscriptionCreateFragment;
@@ -22,8 +26,11 @@ import com.scalified.fab.ActionButton;
 import java.util.ArrayList;
 import java.util.List;
 
+import static xdroid.core.Global.getContext;
+
 public class EventListActivity extends AppCompatActivity implements EventListFragment.OnListFragmentInteractionListener {
 
+    private static final String TAG = "EventListActivity";
     private EventListFragment mFragment;
     private ActionButton mFab;
 
@@ -37,21 +44,50 @@ public class EventListActivity extends AppCompatActivity implements EventListFra
         ArrayList<Event> events = new ArrayList<>();
         if(extras != null) {
             if(extras.containsKey("event-list")){
+                // if an event list is provided
                 events = (ArrayList<Event>) extras.getSerializable("event-list");
-                sample = false;
-            }
-        }
-        if(sample){
-            generateSampleList();
-        } else {
-            mFragment = EventListFragment.newInstance(1, events);
+                mFragment = EventListFragment.newInstance(1, events);
 
-            // initialize the fragment
-            FragmentManager fragmentManager = getSupportFragmentManager();
-            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-            fragmentTransaction.add(R.id.event_list_fragment, mFragment);
-            fragmentTransaction.commit();
+            } else if (extras.containsKey("UID")){
+                // if an uid is provided
+                mFragment = new EventListFragment();
+                final ProgressDialog progress = new ProgressDialog(this);
+                progress.setTitle("Loading");
+                progress.setMessage("Wait while loading...");
+                progress.setCancelable(false); // disable dismiss by tapping outside of the dialog
+                progress.show();
+                NSService.getInstance(getApplicationContext()).getEventsByUser(extras.getString("UID"), new NSService.MyCallback<List<Event>>() {
+                    @Override
+                    public void onSuccess(List<Event> events) {
+                        Log.d(TAG, "events from UID: found "+events.size()+ " events");
+                        RecyclerView recyclerView = mFragment.getRecyclerView();
+                        recyclerView.swapAdapter(new MyEventRecyclerViewAdapter(events, EventListActivity.this, getContext()), false);
+                        progress.cancel();
+                    }
+
+                    @Override
+                    public void onFailure() {
+                        Log.w(TAG, "events from UID: failure");
+                        progress.cancel();
+                    }
+
+                    @Override
+                    public void onMessageLoad(MyMessage message, int status) {
+                        Log.w(TAG, "events from UID: "+message);
+                        progress.cancel();
+                    }
+                });
+            }
+        } else {
+            // if nothing is provided
+            mFragment = new EventListFragment();
         }
+
+        // initialize the fragment
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.event_list_fragment, mFragment);
+        fragmentTransaction.commit();
 
         mFab = (ActionButton) findViewById(R.id.event_create_fab);
         mFab.setOnClickListener(new View.OnClickListener() {
