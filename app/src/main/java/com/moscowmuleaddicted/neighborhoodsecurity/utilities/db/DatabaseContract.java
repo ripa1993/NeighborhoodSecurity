1346727e4ae176b877fd4c31cd8370a6de7495f5
ripa1993 <simone.ripamonti.1993@gmail.com>
2017-05-11 20:54:03 +0200

    Event and Subscription DB first implementation

---
+++ app/src/main/java/com/moscowmuleaddicted/neighborhoodsecurity/utilities/db/DatabaseContract.java
@@ -0,0 +1,80 @@
+package com.moscowmuleaddicted.neighborhoodsecurity.utilities.db;
+
+import android.provider.BaseColumns;
+
+/**
+ * Helper class to hold prepared statements and table / column names
+ *
+ * Created by Simone Ripamonti on 11/05/2017.
+ */
+
+public class DatabaseContract {
+
+    private DatabaseContract() {
+
+    }
+
+    public static class EventEntry implements BaseColumns {
+        public static final String TABLE_NAME = "events";
+        public static final String COLUMN_NAME_DATE = "date";
+        public static final String COLUMN_NAME_EVENTTYPE = "eventtype";
+        public static final String COLUMN_NAME_DESCRIPTION = "description";
+        public static final String COLUMN_NAME_COUNTRY = "country";
+        public static final String COLUMN_NAME_CITY = "city";
+        public static final String COLUMN_NAME_STREET = "street";
+        public static final String COLUMN_NAME_LATITUDE = "latitude";
+        public static final String COLUMN_NAME_LONGITUDE = "longitude";
+        public static final String COLUMN_NAME_SUBMITTERID = "submitterid";
+    }
+
+    public static class SubscriptionEntry implements BaseColumns {
+        public static final String TABLE_NAME = "subscriptions";
+        public static final String COLUMN_NAME_USERID = "userid";
+        public static final String COLUMN_NAME_MIN_LATITUDE = "min_latitude";
+        public static final String COLUMN_NAME_MAX_LATITUDE = "max_latitude";
+        public static final String COLUMN_NAME_MIN_LONGITUDE = "min_longitude";
+        public static final String COLUMN_NAME_MAX_LONGITUDE = "max_longitude";
+        public static final String COLUMN_NAME_RADIUS = "radius";
+        public static final String COLUMN_NAME_COUNTRY = "country";
+        public static final String COLUMN_NAME_CITY = "city";
+        public static final String COLUMN_NAME_STREET = "street";
+    }
+
+    public static class EventStatements {
+        public static final String SQL_CREATE_TABLE_ENTRIES =
+                "CREATE TABLE " + EventEntry.TABLE_NAME + " ("
+                        + EventEntry._ID + " INTEGER PRIMARY KEY, "
+                        + EventEntry.COLUMN_NAME_DATE + " TIMESTAMP, "
+                        + EventEntry.COLUMN_NAME_EVENTTYPE + " TEXT, "
+                        + EventEntry.COLUMN_NAME_DESCRIPTION + " TEXT, "
+                        + EventEntry.COLUMN_NAME_COUNTRY + " TEXT, "
+                        + EventEntry.COLUMN_NAME_CITY + " TEXT, "
+                        + EventEntry.COLUMN_NAME_STREET + " TEXT, "
+                        + EventEntry.COLUMN_NAME_LATITUDE + " DOUBLE, "
+                        + EventEntry.COLUMN_NAME_LONGITUDE + " DOUBLE, "
+                        + EventEntry.COLUMN_NAME_SUBMITTERID + " TEXT )";
+
+        public static final String SQL_DELETE_ENTRIES =
+                "DROP TABLE IF EXISTS " + EventEntry.TABLE_NAME;
+
+    }
+
+    public static class SubscriptionStatements {
+        public static final String SQL_CREATE_TABLE_ENTRIES =
+                "CREATE TABLE " + SubscriptionEntry.TABLE_NAME + " ("
+                        + SubscriptionEntry._ID + " INTEGER PRIMARY KEY, "
+                        + SubscriptionEntry.COLUMN_NAME_USERID + " TEXT, "
+                        + SubscriptionEntry.COLUMN_NAME_MIN_LATITUDE + " DOUBLE, "
+                        + SubscriptionEntry.COLUMN_NAME_MAX_LATITUDE + " DOUBLE, "
+                        + SubscriptionEntry.COLUMN_NAME_MIN_LONGITUDE + " DOUBLE, "
+                        + SubscriptionEntry.COLUMN_NAME_MAX_LONGITUDE + " DOUBLE, "
+                        + SubscriptionEntry.COLUMN_NAME_RADIUS + " INTEGER, "
+                        + SubscriptionEntry.COLUMN_NAME_COUNTRY + " TEXT, "
+                        + SubscriptionEntry.COLUMN_NAME_CITY + " TEXT, "
+                        + SubscriptionEntry.COLUMN_NAME_STREET + " TEXT )";
+
+        public static final String SQL_DELETE_ENTRIES =
+                "DROP TABLE IF EXISTS " + SubscriptionEntry.TABLE_NAME;
+
+    }
+}
---
+++ app/src/main/java/com/moscowmuleaddicted/neighborhoodsecurity/utilities/db/EventDB.java
@@ -0,0 +1,36 @@
+package com.moscowmuleaddicted.neighborhoodsecurity.utilities.db;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by Simone Ripamonti on 11/05/2017.
+ */
+
+public class EventDB extends SQLiteOpenHelper {
+    // If you change the database schema, you must increment the database version.
+    public static final int DATABASE_VERSION = 1;
+    public static final String DATABASE_NAME = "Event.db";
+
+    public EventDB(Context context){
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(DatabaseContract.EventStatements.SQL_CREATE_TABLE_ENTRIES);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // This database is only a cache for online data, so its upgrade policy is
+        // to simply to discard the data and start over
+        db.execSQL(DatabaseContract.EventStatements.SQL_DELETE_ENTRIES);
+        onCreate(db);
+    }
+
+    public void onDowngrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        onUpgrade(db, oldVersion, newVersion);
+    }
+}
